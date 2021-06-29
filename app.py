cabbcaa2a8820c14338431d8ff74742a7483f072
penguin72487 <70788551+penguin72487@users.noreply.github.com>
2021-06-29 11:11:24 +0800

    debug

--- app.py
+++ app.py
@@ -68,9 +68,9 @@ def handle_message(event):
     elif '功能列表' in msg:
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
-    else:
-        message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
+   # else:
+      #  message = TextSendMessage(text=msg)
+      #  line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
