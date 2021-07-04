3e76999bfb95b10f319d268e7afb9740da922c0d
penguin72487 <70788551+penguin72487@users.noreply.github.com>
2021-07-04 21:23:30 +0800

    怕.jpg

--- app.py
+++ app.py
@@ -68,8 +68,11 @@ def handle_message(event):
             message = ImageSendMessage(original_content_url = web_Rul,
             preview_image_url = web_Rul )
         elif '渣男.jpg' in msg :
-            #line_bot_api.reply_message(event.reply_token, message='圖片支援')
             web_Rul = "https://i.imgur.com/fxzh9PQ.jpg"
+            message = ImageSendMessage(original_content_url= web_Rul,
+            preview_image_url = web_Rul )
+        elif '怕.jpg' in msg :
+            web_Rul = "https://i.imgur.com/fu7SJKF.jpg"
             message = ImageSendMessage(original_content_url= web_Rul,
             preview_image_url = web_Rul )
         elif '香.jpg' in msg or '香圖.jpg' in msg :
